b712194c207c6ac1924d9deeb599408a9f8f423f
Chhunkea <kimchhunkea64@gmail.com>
2024-06-26 17:10:16 +0700

    Add: add header component comment

--- components/header.js
+++ components/header.js
@@ -8,8 +8,10 @@ class headerElement extends HTMLElement {
         this.innerHTML = 
         `
           <header>
+            <!-- Header: header Background -->
                 <div class="header-background">
                     <div class="header-content">
+                        <!-- Greeting section -->
                         <div class="greeting">
                             <p>
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
